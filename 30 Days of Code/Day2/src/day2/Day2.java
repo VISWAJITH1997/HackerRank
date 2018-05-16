64a74f80ba926a25b716df3a9224a62506ddd5bc
MAVULURU VISWAJITH REDDY <viswajithreddy1997@gmail.com>
2018-05-16 11:24:33 +0530

    Day 2: Operators

---
+++ 30 Days of Code/Day2/src/day2/Day2.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author viswajith reddy
+ */
+public class Day2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        double meal_cost = in.nextDouble();
+        int tip_percent = in.nextInt();
+        int tax_percent = in.nextInt();
+
+        double tip = meal_cost * tip_percent / 100;
+        double tax = meal_cost * tax_percent / 100;
+
+        Double totalCost = new Double(meal_cost + tip + tax);
+        int roundTotalCost = totalCost.intValue();
+
+        System.out.println("The total meal cost is " + roundTotalCost + " dollars.");
+
+        in.close();
+
+    }
+
+}
