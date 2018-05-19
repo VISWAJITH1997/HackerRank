0dab7579b4a972336d5231f9ac6e7370883bceb4
MAVULURU VISWAJITH REDDY <viswajithreddy1997@gmail.com>
2018-05-19 09:06:13 +0530

    Day 5: Loops

---
+++ 30 Days of Code/Day5/src/day5/Day5.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author viswajith reddy
+ */
+public class Day5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n = scanner.nextInt();
+        scanner.close();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + n * i);
+        }
+
+    }
+}
