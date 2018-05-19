58bf2d85a67aa004059a44aa4ff20a25b9a0bbcf
MAVULURU VISWAJITH REDDY <viswajithreddy1997@gmail.com>
2018-05-19 08:50:38 +0530

    Day 4: Class vs. Instance

---
+++ 30 Days of Code/Day4/src/day4/Day4.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author viswajith reddy
+ */
+public class Day4 {
+
+    int age;
+
+    /**
+     * @param args the command line arguments
+     */
+    // TODO code application logic here
+    public Day4(int initialAge) {
+        // Add some more code to run some checks on initialAge
+        if (initialAge < 0) {
+            System.out.println("Age is not valid, setting age to 0.");
+            age = 0;
+        } else {
+            age = initialAge;
+        }
+    }
+
+    public void amIOld() {
+        // Write code determining if this person's age is old and print the correct statement:
+        if (age < 13) {
+            System.out.println("You are young.");
+        } else if (age >= 13 && age < 18) {
+            System.out.println("You are a teenager.");
+        } else {
+            System.out.println("You are old.");
+        }
+    }
+
+    public void yearPasses() {
+        // Increment this person's age.
+        age++;
+    }
+
+    // Fixed code application logic
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            int age = sc.nextInt();
+            Day4 p = new Day4(age); //person to be used as classname but due to filename i used Day4
+            p.amIOld();
+            for (int j = 0; j < 3; j++) {
+                p.yearPasses();
+            }
+            p.amIOld();
+            System.out.println();
+        }
+        sc.close();
+    }
+
+}
